b729b22782c4aec15870ae348a2b7b34d73a0296
rteina <rteina@galigeo.com>
2019-09-27 17:21:58 +0200

    get models and metadata

--- api/ml/ml-api.js
+++ api/ml/ml-api.js
@@ -12,6 +12,39 @@ router.get('/', (req, res, next) => {
   res.json({ message: '[GET] /api/ml' });
 });
 
+router.get('/models', (req, res, next) => {
+  const { BigQuery } = require('@google-cloud/bigquery');
+  const bigquery = new BigQuery();
+  const dataSetId = 'training_bqml';
+  const dataset = bigquery.dataset(dataSetId);
+
+  dataset.getModels().then(data => {
+    const models = data[0];
+    console.log('Models:');
+    let resModels = [];
+    models.forEach(model => {
+      console.log(model.metadata);
+      resModels.push(model.metadata);
+    });
+    res.json({ models: resModels });
+  });
+});
+
+router.get('/models/:modelId', async (req, res, next) => {
+  const { BigQuery } = require('@google-cloud/bigquery');
+  const bigquery = new BigQuery();
+  const dataSetId = 'training_bqml';
+  const dataset = bigquery.dataset(dataSetId);
+  const [model] = await dataset.model(req.params.modelId).get();
+
+  console.log('Model:');
+  console.log(model.metadata.modelReference);
+  res.json({
+    model: model.metadata.modelReference,
+    featureColumns: model.metadata.featureColumns
+  });
+});
+
 /**
  * Errors on "/api/ml/*" routes.
  */
